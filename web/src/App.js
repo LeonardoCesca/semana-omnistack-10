1218ea3b7cd1f16acfe23405dc05121ed0a33095
LeonardoCesca <leonardocescaf@gmail.com>
2020-01-17 15:52:48 -0300

    add: add dev into db

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api.js';
 
 import './global.css';
 import './App.css';
@@ -30,11 +31,25 @@ function App() {
     )
   }, []);
 
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username, 
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input 
